9e8095a208b9b318432485fcaebc610ccb510376
George Rusu <georgerusu017@yahoo.com>
2023-04-21 09:48:21 +0300

    New Changes

--- playground.js
+++ playground.js
@@ -1,63 +1,51 @@
+// Work with modules
+
 // create div
-function addDiv(id,whereTo,classid = 0) {
+function addDiv(id,whereTo,className = null) {
     const newDiv = document.createElement("div");
     newDiv.setAttribute("id",id)
-    newDiv.setAttribute("class",classid)
+    if (className !== null) newDiv.setAttribute("class",className)
     const app = document.getElementById(whereTo);
     app.appendChild(newDiv);
 }
+
 // create text
-function addText(classid,whereTo,text) {
+function addText(className,whereTo,text) {
     const newDiv = document.createElement("label");
     const newContent = document.createTextNode(text);
     newDiv.appendChild(newContent);
-    newDiv.setAttribute("class",classid)
+    newDiv.setAttribute("class",className)
     const app = document.getElementById(whereTo);
     app.appendChild(newDiv);
 }
+
 // create button
-function addButton(id,classid,whereTo,text) {
+function addButton(id,className,whereTo,text) {
     const newButton = document.createElement("button");
     newButton.setAttribute("id",id)
-    newButton.setAttribute("class",classid)
+    newButton.setAttribute("class",className)
     newButton.innerText = text;
     const app = document.getElementById(whereTo);
     app.appendChild(newButton);
 }
 
 // add img to buttons
-function addImg(whereTo,source,color) {
+function addImg(whereTo,source) {
     const newImg = document.createElement("img");
     newImg.setAttribute("src",source)
-    newImg.setAttribute("color",color)
     newImg.setAttribute("width","30px")
     newImg.setAttribute("height","30px")
     const app = document.getElementById(whereTo);
     app.appendChild(newImg);
-    // app.appendChild(newImg);
 }
-
-// add svg to buttons
-// function addSvg(id,xmlns,classid,whereTo) {
-//     const newSvg = document.createElement("svg");
-//     newSvg.setAttribute("id",id);
-//     newSvg.setAttribute("xmlns",xmlns);
-//     newSvg.setAttribute("class",classid);
-//     const app = document.getElementById(whereTo);
-//     app.appendChild(newSvg);
-
-    // setting path
-    // const newPath = document.createElement("path");
-    // newPath.setAttribute("fill",color);
-    // newPath.setAttribute("d",d)
-    // const app2 = document.getElementById("id",id);
-    // app2.appendChild(newPath);
-// }
 
 // Setting the layout
 addDiv("timer","app")
 addDiv("table","app")
 addDiv("control","app")
+addDiv("control-buttons","control")
+addDiv("numbpad","control")
+
 
 // Adding timer text
 addText("text","timer","Timer: 12:34:56")
@@ -66,40 +54,36 @@ addText("text","timer","Timer: 12:34:56")
 //WORKING ON CONTROL PANEL
 // Adding round buttons
 for(let i=1; i<=3; i++){
-    addButton("round-"+i,"round_buttons","control","")
+    addButton("round-"+i,"round_buttons","control-buttons","")
 }
 
-// addindg svgs to round buttons
-// const undo_button = document.getElementById("round-1");
-// undo_button.setAttribute("svg",)
-// const erase_button = document.getElementById("round-2");
-// const notes_button = document.getElementById("round-3");
-addImg("round-1","./images/undo.png","#0072e3")
-
+// Almost working
+addImg("round-1","./images/undo.png")
+addImg("round-2","./images/eraser.png")
+addImg("round-3","./images/pencil.png")
 
 // Adding round buttons labels
-addText("control_text","control","Undo")
-addText("control_text","control","Erase")
-addText("control_text","control","Notes")
-// addButton("round_buttons","control")
-// addButton("round_buttons","control")
-// addButton("round_buttons","control")
+addText("control_text","control-buttons","Undo")
+addText("control_text","control-buttons","Erase")
+addText("control_text","control-buttons","Notes")
 
 
 // Adding square buttons
 for(let i=1; i<=9; i++){
-    addButton("square-"+i,"number_buttons","control",i)
+    addButton("number-button-"+i,"number_buttons","numbpad",i)
 }
 
-addButton("newGame","newGame","control","New Game")
+addButton("newGame","newGame","numbpad","New Game")
 
 
 // WORKING ON TABLE
 
+
+// square is now cell
 // Adding squares to the table
-for(let j=0; j<=8; j++){
-    addDiv("bigSquare-"+(j+1),"table","bigSquare")
+for(let j=1; j<=9; j++){
+    addDiv("bigSquare-"+(j),"table","bigSquare")
     for (let i=1; i<=9; i++){
-        addDiv("square-"+((9*j)+i),"bigSquare-"+(j+1),"square")
+        addDiv("cell-"+((9*(j-1))+i),"bigSquare-"+(j),"cell")
     }
 }
