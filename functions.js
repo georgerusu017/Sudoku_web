0d7f9f8dc678872da43950f7bf19cb560671931c
George Rusu <georgerusu017@yahoo.com>
2023-04-25 12:26:35 +0300

    new look

---
+++ functions.js
@@ -0,0 +1,86 @@
+// Functions needed in the app
+
+function addDiv(id, whereTo, className = null) {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id", id)
+    if (className !== null) newDiv.setAttribute("class", className)
+    const app = document.getElementById(whereTo);
+    app.appendChild(newDiv);
+}
+
+function addText(className, whereTo, text) {
+    const newDiv = document.createElement("label");
+    const newContent = document.createTextNode(text);
+    newDiv.appendChild(newContent);
+    newDiv.setAttribute("class", className)
+    const app = document.getElementById(whereTo);
+    app.appendChild(newDiv);
+}
+
+function addButton(id, className, whereTo, text) {
+    const newButton = document.createElement("button");
+    newButton.setAttribute("id", id);
+    newButton.setAttribute("class", className);
+    newButton.innerText = text;
+    const app = document.getElementById(whereTo);
+    app.appendChild(newButton);
+}
+
+function addImg(whereTo, source) {
+    const newImg = document.createElement("img");
+    newImg.setAttribute("src", source);
+    newImg.setAttribute("width", "30px");
+    newImg.setAttribute("height", "30px");
+    const app = document.getElementById(whereTo);
+    app.appendChild(newImg);
+}
+
+function getCellId(position) {
+    return `cell-${position}`;
+}
+
+function populateTable(puzzleValues) {
+    // works only if the populated location is named "cell-#" where # is a number
+    const gridSize = 81;
+    if (puzzleValues.length !== gridSize) {
+        return `The array length must be ${gridSize}`;
+    }
+    for (i = 0; i < gridSize; i++) {
+        const cellId = getCellId(i);
+        const cellValue = puzzleValues[i] == "." ? "" : puzzleValues[i];
+        addText("value", cellId, cellValue);
+    }
+}
+
+function createSudokuGrid() {
+    function createSquareLine(squareId, i, k) {
+        for (let j = 0; j < 3; j++) {
+            addDiv(getCellId((9 * k) + (3 * i) + j), squareId, "cell");
+        }
+    }
+
+    for (let i = 0; i < 9; i++) {
+        const squareId = "Square-" + (i);
+        addDiv(squareId, LAYOUT_IDS.table, "Square")
+
+        if (i < 3) {
+            for (k = 0; k <= 2; k++) {
+                createSquareLine(squareId, i, k);
+            }
+        } else if (i < 6) {
+            for (k = 2; k <= 4; k++) {
+                createSquareLine(squareId, i, k);
+            }
+        } else if (i < 9) {
+            for (k = 4; k <= 6; k++) {
+                createSquareLine(squareId, i, k);
+            }
+        }
+    }
+}
+
+function createLayout(where,...args) {
+    args.forEach((arg) => {
+        addDiv(arg, where)
+    });
+}
--- playground.js
+++ playground.js
@@ -1,138 +1,50 @@
 // Work with modules
 
-// create div
-function addDiv(id,whereTo,className = null) {
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute("id",id)
-    if (className !== null) newDiv.setAttribute("class",className)
-    const app = document.getElementById(whereTo);
-    app.appendChild(newDiv);
+const LAYOUT_IDS = {
+    timer: "timer",
+    table: "table",
+    control: "control",
+    controlButtons: "control-buttons",
+    numbpad: "numbpad"
+}
+const CONTROL_IDS = {
+    undoButton: "undoButton",
+    eraseButton: "eraseButton",
+    notesButton: "notesButton",
+    undoLabel: "undoLabel",
+    eraseLabel: "eraseLabel",
+    notesLabel: "notesLabel",
 }
 
-// create text
-function addText(className,whereTo,text) {
-    const newDiv = document.createElement("label");
-    const newContent = document.createTextNode(text);
-    newDiv.appendChild(newContent);
-    newDiv.setAttribute("class",className)
-    const app = document.getElementById(whereTo);
-    app.appendChild(newDiv);
-}
+createLayout("app",LAYOUT_IDS.timer,LAYOUT_IDS.table,LAYOUT_IDS.control)
+createLayout("control", LAYOUT_IDS.controlButtons, "numbpad")
 
-// create button
-function addButton(id,className,whereTo,text) {
-    const newButton = document.createElement("button");
-    newButton.setAttribute("id",id);
-    newButton.setAttribute("class",className);
-    newButton.innerText = text;
-    const app = document.getElementById(whereTo);
-    app.appendChild(newButton);
-}
+addText("text", "timer", "Timer: 12:34:56")
 
-// add img to buttons
-function addImg(whereTo,source) {
-    const newImg = document.createElement("img");
-    newImg.setAttribute("src",source);
-    newImg.setAttribute("width","30px");
-    newImg.setAttribute("height","30px");
-    const app = document.getElementById(whereTo);
-    app.appendChild(newImg);
-}
+createLayout(LAYOUT_IDS.controlButtons,"undo-div","erase-div","notes-div")
 
-function populateTable(arr){
-    // works only if the populated location is named "cell-#" where # is a number
+addButton(CONTROL_IDS.undoButton,"round_buttons","undo-div",'')
+addButton(CONTROL_IDS.eraseButton,"round_buttons","erase-div",'')
+addButton(CONTROL_IDS.notesButton,"round_buttons","notes-div",'')
 
-    if(arr.length !== 81) return "The array length must be 81";
-
-    for(i = 0; i<81; i++){
-        if(arr[i] == ".") addText("value",'cell-'+i,"")
-        else addText("value",'cell-'+i,arr[i])
-    }
-}
-
-function createTableContent(){
-    for(let i=0; i<3; i++){
-        addDiv("Square-"+(i),"table","Square")
-        let k = 0;
-        for(let j=0; j<3; j++){
-            addDiv("cell-"+((9*k)+(3*i)+j),"Square-"+(i),"cell")
-        }
-        k += 1;
-        for(let j=0; j<3; j++){
-            addDiv("cell-"+((9*k)+(3*i)+j),"Square-"+(i),"cell")
-        }
-        k += 1;
-        for(let j=0; j<3; j++){
-            addDiv("cell-"+((9*k)+(3*i)+j),"Square-"+(i),"cell")
-        }
-    }
-    for(let i=3; i<6; i++){
-        addDiv("Square-"+(i),"table","Square")
-        let k = 2;
-        for(let j=0; j<3; j++){
-            addDiv("cell-"+((9*k)+(3*i)+j),"Square-"+(i),"cell")
-        }
-        k += 1;
-        for(let j=0; j<3; j++){
-            addDiv("cell-"+((9*k)+(3*i)+j),"Square-"+(i),"cell")
-        }
-        k += 1;
-        for(let j=0; j<3; j++){
-            addDiv("cell-"+((9*k)+(3*i)+j),"Square-"+(i),"cell")
-        }
-    }
-    for(let i=6; i<9; i++){
-        addDiv("Square-"+(i),"table","Square")
-        let k = 4;
-        for(let j=0; j<3; j++){
-            addDiv("cell-"+((9*k)+(3*i)+j),"Square-"+(i),"cell")
-        }
-        k += 1;
-        for(let j=0; j<3; j++){
-            addDiv("cell-"+((9*k)+(3*i)+j),"Square-"+(i),"cell")
-        }
-        k += 1;
-        for(let j=0; j<3; j++){
-            addDiv("cell-"+((9*k)+(3*i)+j),"Square-"+(i),"cell")
-        }
-    }
-}
-
-// Setting the layout
-addDiv("timer","app")
-addDiv("table","app")
-addDiv("control","app")
-addDiv("control-buttons","control")
-addDiv("numbpad","control")
-
-// Adding timer text
-addText("text","timer","Timer: 12:34:56")
-
-//WORKING ON CONTROL PANEL
-// Adding round buttons
-for(let i=1; i<=3; i++){
-    addButton("round-"+i,"round_buttons","control-buttons","")
-}
-
-// Adding Round buttons to numpad
-addImg("round-1","./images/undo.png")
-addImg("round-2","./images/eraser.png")
-addImg("round-3","./images/pencil.png")
+addImg(CONTROL_IDS.undoButton, "./images/undo.png")
+addImg(CONTROL_IDS.eraseButton, "./images/eraser.png")
+addImg(CONTROL_IDS.notesButton, "./images/pencil.png")
 
 // Adding round buttons labels
-addText("control_text","control-buttons","Undo")
-addText("control_text","control-buttons","Erase")
-addText("control_text","control-buttons","Notes")
+addText("control_text", "undo-div", "Undo")
+addText("control_text", "erase-div", "Erase")
+addText("control_text", "notes-div", "Notes")
 
 
 // Adding square buttons
-for(let i=1; i<=9; i++){
-    addButton("number-button-"+i,"number_buttons","numbpad",i)
+for (let i = 1; i <= 9; i++) {
+    addButton("number-button-" + i, "number_buttons", "numbpad", i)
 }
 
-addButton("newGame","newGame","numbpad","New Game")
+addButton("newGame", "newGame", "numbpad", "New Game")
 
-createTableContent();
+createSudokuGrid();
 
 let sudokuUnsolved = sudoku.generate("medium");
 let sudokuUnsolvedGrid = sudoku.board_string_to_grid(sudokuUnsolved);
