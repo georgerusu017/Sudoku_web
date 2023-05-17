ef442597be254a771cf6e14a1bb02af6bbbaac5e
George Rusu <58792068+georgerusu017@users.noreply.github.com>
2023-05-17 11:52:15 +0300

    Yes

--- constants.js
+++ constants.js
@@ -13,7 +13,7 @@ const LAYOUT_ID = Object.freeze({
     controlButtons: "control-buttons",
     numpad: "numbpad"
 })
-const CONTROL_ID = {
+const CONTROL_ID = Object.freeze({
     undoButton: "undoButton",
     eraseButton: "eraseButton",
     notesButton: "notesButton",
@@ -21,28 +21,9 @@ const CONTROL_ID = {
     eraseLabel: "eraseLabel",
     notesLabel: "notesLabel",
     newGameButton: 'newGame',
-    //
-    numberButtons: []
-    // [ ...Array(N).keys() ]
-    // Array(N).fill().map((_, i) => i+1);
-    // Array(10).fill().map((_, i) => i+1);
-}
+    numberButtons: [...Array(9).fill().map((_, i) => `number-button-${i+1}`)],
 
-const SQUARES_IDS = [
-  [0,1,2,9,10,11,18,19,20],
-  [3,4,5,12,13,14,21,22,23],
-  [6,7,8,15,16,17,24,25,26],
-  [27,28,29,36,37,38,45,46,47],
-  [30,31,32,39,40,41,48,49,50],
-  [33,34,35,42,43,44,51,52,53],
-  [54,55,56,63,64,65,72,73,74],
-  [57,58,59,66,67,68,75,76,77],
-  [60,61,62,69,70,71,78,79,80]
-];
-
-for (let i = 1; i < 10; i++) {
-    CONTROL_ID.numberButtons.push(`number-button-${i}`)
-}
+})
 
 const ARROW_KEY = Object.freeze({
     up: "ArrowUp",
@@ -51,4 +32,4 @@ const ARROW_KEY = Object.freeze({
     right: "ArrowRight",
 })
 
-export { CELL_CSS, LAYOUT_ID, CONTROL_ID, ARROW_KEY, SQUARES_IDS } 
+export { CELL_CSS, LAYOUT_ID, CONTROL_ID, ARROW_KEY } 
